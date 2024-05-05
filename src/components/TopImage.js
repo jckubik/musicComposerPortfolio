4dcd473d9a1394344cc6787ba689fc371ab0fb66
Jacob <jckubik@icloud.com>
2024-05-05 13:15:23 -0600

    Remove warning causing build failure in TopImage

--- src/components/TopImage.js
+++ src/components/TopImage.js
@@ -1,7 +1,4 @@
 import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import topImage from '../media/topImage.png';
 import '../css/topImage.css';
 
 const TopImage = () => {
