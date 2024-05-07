7c43e6bee53143a1e13022db7407a20d0abd48f8
Jacob <jckubik@icloud.com>
2024-05-07 14:57:13 -0600

    Remove unneeded import to fix build

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,6 @@
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import aboutPicture from '../media/aboutPhoto.png';
 import '../css/about.css';
 
 const About = () => {
