d6531833b82fdd46c96f2ce736e2c30ed722f098
Jacob <jckubik@icloud.com>
2024-05-06 21:28:41 -0600

    Add title to top image. Update social icon spacing

--- src/components/SocialIcons.js
+++ src/components/SocialIcons.js
@@ -7,19 +7,19 @@ import '../css/socialIcons.css'
 const SocialIcons = ({ iconColorClassName, iconHoverColor }) => {
 
   return (
-    <Col md={2} className='icon-container align-content-center'>
+    <Col md={8} className='icon-container align-content-center'>
       <Row>
-        <Col md={4}>
+        <Col md={2}>
           <a className={`${iconColorClassName} ${iconHoverColor}`} href ="https://www.youtube.com/@thedahmusicstudio29">
             <FontAwesomeIcon icon={faYoutube} size='xl' />
           </a>
         </Col>
-        <Col md={4}>
+        <Col md={2}>
           <a className={`${iconColorClassName} ${iconHoverColor}`} href ="https://www.instagram.com/musicby_dah/">
             <FontAwesomeIcon icon={faInstagram} size='xl' />
           </a>
         </Col>
-        <Col md={4}>
+        <Col md={2}>
           <a className={`${iconColorClassName} ${iconHoverColor}`} href ="https://soundcloud.com/dahmusicstudio?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing">
             <FontAwesomeIcon icon={faSoundcloud} size='xl' />
           </a>
--- src/components/TopImage.js
+++ src/components/TopImage.js
@@ -1,17 +1,19 @@
 import Container from 'react-bootstrap/Container';
 import '../css/topImage.css';
+import { Col, Row } from 'react-bootstrap';
 
 const TopImage = () => {
 
   return (
     <Container id="topImage-container" className='make-page background-image-pic'>
-      {/* <img 
-        src={topImage}
-        alt='Portrait of Dmitri Hunter'
-        height={"auto"}
-        width={"1200"}
-        className='m-5'
-      /> */}
+      <Row>
+        <Col>
+        <div className='intro-div'>
+          <h1 className='text-center'>Dmitri Hunter</h1>
+          <p className='text-center secondary-title'>Film | Video Game | Media Composer</p>
+        </div>
+        </Col>
+      </Row>
     </Container>
   );
 };
